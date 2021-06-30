fd6c7e80477e0cc82d7065c4505af86653719d37
piseth <piseth@test.com>
2021-06-30 13:23:17 +0700

    add files to repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.86.0',
+      'version' => '7.86.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7a56d1066305afe2601f39bed5b1caf277f829c7',
+    ),
+  ),
+);
